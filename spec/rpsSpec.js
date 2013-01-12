cdb084f37769245e1dc12fb67bfeaef88e2093f5
Hugo Estrada <hugoestr@gmail.com>
2013-01-11 19:11:09 -0500

    moved out the RockPaperScissor object to its own file. Started to write specs.

--- spec/rpsSpec.js
+++ spec/rpsSpec.js
@@ -1,14 +1,33 @@
-describe("Strategy", function(){
+describe("RockPaperScissors", function(){
 	var rps;
 
 	beforeEach(function(){
-		rps = new Strategy();
+       		rps = new RockPaperScissors();
 	});
 
-	it("should give you an answer", function(){
-		var answer = rps.answer();
-		expect(answer).not.toBeNull();
+
+	it("has playing set to true", function(){
+		expect(rps.playing).toBe(true);
 	});
+
+	describe("when playing a winning move", function(){
+	   beforeEach(function(){
+		rps.playTurn("scissors");
+	   });
+
+	   it("computer_money increases to $110 ", function(){
+		expect(rps.player_money).toEqual(110);
+		});
+	
+	});
+
+	describe("when losing", function(){
+		it("should set playing to false", function(){
+			rps.player_money = 0;
+			rps.playTurn("rock");
+			expect(rps.playing).toBe(false);
+		});
+	});
+
 });
 
-describe
--- src/rps.js
+++ src/rps.js
@@ -1,7 +1,67 @@
-function Strategy(){
-  
-}
+	function RockPaperScissors(){
+			this.winner = false;
+			this.playing = true;
+			
+			this.player_money = 100;
+			this.computer_money = 100;
+			this.outcome = "";
+			
+			this.tries = 0;
+			this.won = 0;
+		}
 
-Rps.prototype.answer = function(){
-	return "paper";
-}
+		RockPaperScissors.prototype.playTurn = function(player){
+				 var computer, outcome, that;
+				 this.tries++;
+				 
+				 this.computer = simpletonStrategy();
+         this.outcome = game_outcome(player, this.computer);
+				 
+			   that = this;	 
+				 update_score(that);
+				 check_winning_conditions(this);	 
+
+
+			 function game_outcome(player, computer){
+				var outcomes = {
+					"paper" : {"rock" : "win" , "paper": "tie", "scissors": "lose" },
+					"rock" : {"rock" : "tie" , "paper": "lose", "scissors": "win" },
+					"scissors" : {"rock" : "lose" , "paper": "win", "scissors": "tie" },
+				};
+
+				return outcomes[player][computer];		
+			 }
+			 
+			 function simpletonStrategy(){
+				return "paper";		
+			}
+			 
+			 function update_score(rps){
+						if (rps.outcome == "win") {
+							rps.won++;
+							rps.player_money += 10;
+							rps.computer_money -= 10;
+						}
+
+						if (rps.outcome == "lose"){
+							rps.player_money -= 10;
+							rps.computer_money += 10;
+						}
+			 } 
+			
+			 function check_winning_conditions(rps){
+ 					if (rps.player_money <= 0 || rps.computer_money <= 0){
+						if (rps.player_money <= 0){
+							rps.winner = "computer";
+						}
+						else {
+						  rps.winner = "player";
+						}
+
+						if (rps.winnmer != ""){
+							rps.playing = false;
+						}
+				 }
+			 }
+		}
+	
